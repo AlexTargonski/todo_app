1525a2279498fb4b32036478781af04cff0fae01
Oleksandr <targonsky97@gmail.com>
2017-04-03 12:26:37 +0300

    autologin after sing up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,5 +19,16 @@
 //= require moment
 //= require bootstrap-datetimepicker
 
+$(document).ready(function(){
+    $('.check:button').toggle(function(){
+        $('input:checkbox').attr('checked','checked');
+        $(this).val('uncheck all')
+    },function(){
+        $('input:checkbox').removeAttr('checked');
+        $(this).val('check all');        
+    })
+})
+
+
 
 
